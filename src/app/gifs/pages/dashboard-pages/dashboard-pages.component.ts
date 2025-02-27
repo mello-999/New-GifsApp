8ef4a81aefbb5f005cfdb675d8a4b74ed2959146
Mauricio donoso <donosomauricio721@gmail.com>
2025-02-27 01:48:35 -0300

    Cap.70-Completo

---
+++ src/app/gifs/pages/dashboard-pages/dashboard-pages.component.ts
@@ -0,0 +1,10 @@
+import {  Component } from '@angular/core';
+import {  RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard-pages',
+  imports: [RouterOutlet],
+  templateUrl: './dashboard-pages.component.html',
+
+})
+export default class DashboardPagesComponent { }
