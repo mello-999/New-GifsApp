714847ffc8e66c0aac214955159defff8e7c2971
Mauricio donoso <donosomauricio721@gmail.com>
2025-02-28 00:29:32 -0300

    Cap.72-Comppleto

--- src/app/gifs/pages/dashboard-pages/dashboard-pages.component.ts
+++ src/app/gifs/pages/dashboard-pages/dashboard-pages.component.ts
@@ -1,9 +1,10 @@
 import {  Component } from '@angular/core';
 import {  RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-dashboard-pages',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenuComponent, ],
   templateUrl: './dashboard-pages.component.html',
 
 })
