07326ae290d693c4cd3fd276d0202abdad2ca698
Mauricio donoso <donosomauricio721@gmail.com>
2025-02-28 03:04:29 -0300

    avanzando

--- src/app/gifs/components/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu-options/side-menu-options.component.ts
@@ -1,9 +1,43 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  sublabel: string;
+}
+
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent {
+
+  menuOptions:MenuOption[] = [
+  {
+    icon: 'fa-solid fa-chart-line',
+    label: 'trending',
+    sublabel: 'Gifs populares',
+    route: '/dashboard/trending',
+  },
+
+  {
+    icon: 'fa-solid fa-magnifying-glass',
+    label: 'Buscador',
+    sublabel: 'Buscar gifs ',
+    route: '/dashboard/search',
+  },
+
+  ];
+
+
+
+
+
+}
 
